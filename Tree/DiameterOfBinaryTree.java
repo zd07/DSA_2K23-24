49f9ecf6f10a365015c2125ce0530d9ed9ffbec3
zd07 <zahidibnrizwan@gmail.com>
2023-11-25 18:38:41 +0530

    DiameterOfBinaryTree  Problem  added

---
+++ Tree/DiameterOfBinaryTree.java
@@ -0,0 +1,18 @@
+package Tree;
+
+public class DiameterOfBinaryTree {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] diameter = new int[1];
+        height(root, diameter);
+        return diameter[0];
+    }
+
+    public int height(TreeNode root, int[] diameter) {
+        if (root == null)
+            return 0;
+        int leftHeight = height(root.left, diameter);
+        int rightHeight = height(root.right, diameter);
+        diameter[0] = Math.max(diameter[0], leftHeight + rightHeight);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
