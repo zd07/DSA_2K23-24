60c9525ffcf0b162ef3eaab8597c4c167938a5f7
zd07 <zahidibnrizwan@gmail.com>
2023-12-01 22:05:58 +0530

    SymmetricTree problem Added

---
+++ Tree/SymmetricTree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return root==null || isSymmetricHelp(root.left,root.right);
+    }
+    public boolean isSymmetricHelp(TreeNode left , TreeNode right){
+        if(left == null || right == null){
+            return left==right;
+        }
+        if(left.val != right.val) return false;
+        return isSymmetricHelp(left.left,right.right) && isSymmetricHelp(left.right , right.left);
+    }
+}
