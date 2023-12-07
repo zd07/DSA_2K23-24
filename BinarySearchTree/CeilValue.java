ed3e97bb0b9ea2522a3c91d0f58b04e4cfb3b8be
zd07 <zahidibnrizwan@gmail.com>
2023-12-07 07:47:58 +0530

    CeilValue Problem Added

---
+++ BinarySearchTree/CeilValue.java
@@ -0,0 +1,21 @@
+package BinarySearchTree;
+
+public class CeilValue {
+    public  static int findCeil(TreeNode node, int x) {
+
+        // Write your code here
+        int ans=-1;
+        while(node!=null){
+            if(node.val==x)return x;
+            if(x>node.val){
+                node=node.right;
+            }
+            else{
+                ans=node.val;
+                node=node.left;
+            }
+        }
+        return ans;
+
+    }
+}
