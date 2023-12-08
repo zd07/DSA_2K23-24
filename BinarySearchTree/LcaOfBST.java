8cc6b8e5c971b901bcf6033a9508d6062062547e
zd07 <zahidibnrizwan@gmail.com>
2023-12-08 23:16:06 +0530

    LCA of BST Problem Added

---
+++ BinarySearchTree/LcaOfBST.java
@@ -0,0 +1,10 @@
+package BinarySearchTree;
+
+public class LcaOfBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right,p,q);
+        if(p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left,p,q);
+        return root;
+    }
+}
